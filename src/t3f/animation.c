e75fa02458153491e22ae584afd2411b70b66945
Todd Cope <todd@t3-i.com>
2025-04-01 15:16:49 -0500

    Upgrade to the latest version of T3F.

--- src/t3f/animation.c
+++ src/t3f/animation.c
@@ -455,7 +455,7 @@ typedef struct
 static void * _t3f_load_actual_animation_data(void * arg)
 {
 	_T3F_ANIMATION_LOAD_INFO * load_info = (_T3F_ANIMATION_LOAD_INFO *)arg;
-	T3F_ANIMATION_DATA * ret;
+	T3F_ANIMATION_DATA * ret = NULL;
 
 	if(arg)
 	{
@@ -681,12 +681,18 @@ int t3f_save_animation(T3F_ANIMATION * ap, const char * fn)
 bool t3f_update_animation(T3F_ANIMATION * ap)
 {
 	int i;
+	T3F_ANIMATION_DATA * new_data;
 
 	if(t3f_object_ready(ap->object_loader))
 	{
-		ap->data = t3f_fetch_object(ap->object_loader);
-		if(ap->data)
+		new_data = t3f_fetch_object(ap->object_loader);
+		if(new_data)
 		{
+			if(new_data != ap->data)
+			{
+				_t3f_destroy_animation_data(ap->data);
+				ap->data = new_data;
+			}
 			_t3f_fix_animation_frames(ap);
 			for(i = 0; i < ap->data->bitmaps->count; i++)
 			{
@@ -842,59 +848,6 @@ static void handle_vh_flip(T3F_ANIMATION_FRAME * base_fp, T3F_ANIMATION_FRAME * 
 	{
 		*dflags |= ALLEGRO_FLIP_VERTICAL;
 		*foy = -((fp->y + fp->height) - (base_fp->y + base_fp->height)) - (fp->y - base_fp->y);
-	}
-}
-
-static void _t3f_draw_scaled_rotated_bitmap(ALLEGRO_BITMAP * bp, ALLEGRO_COLOR color, float cx, float cy, float x, float y, float z, float angle, float scale_x, float scale_y, int flags)
-{
-	/* upper left and bottom right points in 3d */
-	float obj_x[2], obj_y[2], obj_z[2], obj_cx, obj_cy;
-	float rx, ry;
-
-	/* upper left and bottom right points in 2d */
-	float screen_w, screen_h;
-
-	obj_x[0] = t3f_project_x(0, z);
-	obj_x[1] = t3f_project_x((float)al_get_bitmap_width(bp) * scale_x, z);
-	obj_y[0] = t3f_project_y(0, z);
-	obj_y[1] = t3f_project_y((float)al_get_bitmap_height(bp) * scale_y, z);
-	obj_z[0] = z + t3f_current_view->virtual_width;
-//	obj_z[1] = z + t3f_virtual_display_width;
-	obj_cx = t3f_project_x(x, z);
-	obj_cy = t3f_project_y(y, z);
-
-	/* clip sprites at z = 0 */
-	if(obj_z[0] > 0)
-	{
-		screen_w = obj_x[1] - obj_x[0];
-		screen_h = obj_y[1] - obj_y[0];
-		rx = screen_w / al_get_bitmap_width(bp);
-		ry = screen_h / al_get_bitmap_height(bp);
-		al_draw_tinted_scaled_rotated_bitmap(bp, color, cx, cy, obj_cx, obj_cy, rx, ry, angle, flags);
-	}
-}
-
-static void _t3f_draw_scaled_bitmap(ALLEGRO_BITMAP * bp, ALLEGRO_COLOR color, float x, float y, float z, float w, float h, int flags)
-{
-	/* upper left and bottom right points in 3d */
-	float obj_x[2], obj_y[2], obj_z[2];
-
-	/* upper left and bottom right points in 2d */
-	float screen_w, screen_h;
-
-	obj_x[0] = t3f_project_x(x, z);
-	obj_x[1] = t3f_project_x(x + w, z);
-	obj_y[0] = t3f_project_y(y, z);
-	obj_y[1] = t3f_project_y(y + h, z);
-	obj_z[0] = z + t3f_current_view->virtual_width;
-//	obj_z[1] = z + t3f_virtual_display_width;
-
-	/* clip sprites at z = 0 */
-	if(obj_z[0] > 0)
-	{
-		screen_w = obj_x[1] - obj_x[0];
-		screen_h = obj_y[1] - obj_y[0];
-		al_draw_tinted_scaled_bitmap(bp, color, 0, 0, al_get_bitmap_width(bp), al_get_bitmap_height(bp), obj_x[0], obj_y[0], screen_w, screen_h, flags);
 	}
 }
 
